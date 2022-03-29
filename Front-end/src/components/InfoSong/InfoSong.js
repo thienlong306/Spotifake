07c80c92f5633e4e6f4431289830413e69dacfd6
thienlong306 <phandongthienlong306@gmail.com>
2022-03-29 22:15:14 +0700

    edit InfoSong default

--- Front-end/src/components/InfoSong/InfoSong.js
+++ Front-end/src/components/InfoSong/InfoSong.js
@@ -6,14 +6,14 @@ class InfoSong extends Component {
       <div className="sidebar__suggest-album">
         <div className="suggest-album">
           <div className="suggest-album__img-container">
-            <img src={this.props.player.imageSongURL} alt="" />
+            <img src={this.props.player.imageSongURL!=null?this.props.player.imageSongURL:require("../../images/null.jpg")} alt="" />
           </div>
           <div className="suggest-album__content">
             <div className="suggest-album__info">
-              <h4>{this.props.player.nameSong!=null?this.props.player.nameSong.split('.')[1]:"null"}</h4>
-              <p>{this.props.player.nameSong!=null?this.props.player.nameSong.split('.')[0]:"null"}</p>
+              <h4>{this.props.player.nameSong!=null?this.props.player.nameSong.split('.')[1]:""}</h4>
+              <p>{this.props.player.nameSong!=null?this.props.player.nameSong.split('.')[0]:""}</p>
             </div>
-            <i className="far fa-plus-square"></i>
+            {/* <i className="far fa-plus-square"></i> */}
           </div>
         </div>
       </div>
