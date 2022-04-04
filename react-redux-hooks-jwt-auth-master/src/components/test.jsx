65bbfd4c486820ad36d39327b714fb0c144d3b0f
boopanda1998 <boopanda1998@gmail.com>
2022-04-04 10:24:20 +0700

    huy
    word bao cao, backend, frontend update

---
+++ react-redux-hooks-jwt-auth-master/src/components/test.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class test extends Component {
+    render() {
+        return (
+            <div>
+                afdsfasdfs
+            </div>
+        );
+    }
+}
+
+export default test;
